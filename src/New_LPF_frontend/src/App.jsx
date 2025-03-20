847f49d3988e19b33694dbccb037728fbdac6dd3
Bada-Don <harshitashwani@gmail.com>
2025-03-20 22:19:36 +0530

    Somewhat working just few more changes

--- src/New_LPF_frontend/src/App.jsx
+++ src/New_LPF_frontend/src/App.jsx
@@ -6,20 +6,23 @@ import AuthPage from './pages/AuthPage';
 import LostPetForm from './pages/LostPetForm';
 import MessagesPage from './pages/MessagesPage';
 import WalletPage from './pages/WalletPage';
-import ConnectionTest from './pages/ConnectionTest'; // Import the test component
+import ConnectionTest from './pages/ConnectionTest';
+import { BackendProvider } from './context/BackendContext'; // Import the provider
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="/petform" element={<LostPetForm />} />
-        <Route path="/messages" element={<MessagesPage />} />
-        <Route path="/wallet" element={<WalletPage />} />
-        <Route path="/test" element={<ConnectionTest />} /> {/* Add the test route */}
-      </Routes>
-    </BrowserRouter>
+    <BackendProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/petform" element={<LostPetForm />} />
+          <Route path="/messages" element={<MessagesPage />} />
+          <Route path="/wallet" element={<WalletPage />} />
+          <Route path="/test" element={<ConnectionTest />} />
+        </Routes>
+      </BrowserRouter>
+    </BackendProvider>
   );
 }
 
